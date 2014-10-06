b83e0f4231132b26d4eb1149c678d346a863e240
ben <brainychip@gmail.com>
2014-10-07 00:56:31 +0300

    A class to handle the display of all places

--- experimental/views.py
+++ experimental/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from experimental.models import Place
 
 # Create your views here.
+
+class PlaceList(ListView):
+    model = Place
+
